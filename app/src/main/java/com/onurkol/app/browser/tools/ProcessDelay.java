645c15ee5fadb0488408d7cc2eec197744a03e11
Onur Kol <onurkol4161@gmail.com>
2022-07-01 02:01:43 +0300

    Release V4.0.0

--- app/src/main/java/com/onurkol/app/browser/tools/ProcessDelay.java
+++
@@ -1,7 +0,0 @@
-package com.onurkol.app.browser.tools;
-
-public class ProcessDelay {
-    public static void Delay(Runnable runnable, int milliseconds){
-        new android.os.Handler().postDelayed(runnable,milliseconds);
-    }
-}
